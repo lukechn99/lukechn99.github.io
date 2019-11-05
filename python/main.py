728ff655b10d8129e1d968677e97eb44f186606b
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2019-11-05 16:57:44 -0600

    experimental

---
+++ python/main.py
@@ -0,0 +1,37 @@
+import scrapy
+from random import randint
+from flask import Flask
+
+## main should include parsing
+## matching to 
+## use scrapy
+
+class nasdaqCrawler(scrapy.Spider):
+    name = "nasdaq"
+    url = input()
+
+    def parse(self, response):
+        for title in response.css
+## PATTERNS
+## Pennant
+#
+#
+## Cup and Handle
+#
+#
+## Ascending Triangle
+#
+#
+## Triple Bottom
+#
+#
+## Descending Triangle
+#
+#
+## Inverse head and shoulders
+#
+#
+## Bullish Symmetric Triangle
+#
+#
+## 
