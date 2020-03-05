8ed8fdf5c5d76b296247e250ec7779ec7a2783a2
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2020-03-05 17:20:20 -0600

    added ifttt and leetcode

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+function show() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  // Add the "show" class to DIV
+  x.className = "show";
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  // After 3 seconds, remove the show class from DIV
+  //setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  
 }
