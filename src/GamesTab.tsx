e15b53fa28d01d6f3cb4438b2b227f766461dc43
Luke Chen <47294899+lukechn99@users.noreply.github.com>
2026-01-30 20:11:12 -0600

    add games link

--- src/GamesTab.tsx
+++ src/GamesTab.tsx
@@ -1,9 +1,15 @@
-import { Stack } from "@mantine/core"
+import { Box, Stack } from "@mantine/core"
 import classes from './GamesTab.module.css'
 
 export default function GamesTab() {
+    const gamesLink = "https://games-alpha-rouge.vercel.app/"
+
     return <Stack align="center">
-        After I finish the WebRTC games I can showcase some learnings there too.
-        <iframe src="https://games-alpha-rouge.vercel.app/" title="Vercel Games" className={classes.iframe}></iframe>
+        After I finish the WebRTC games I can showcase some learnings there too. This is still a work in progress.
+
+        <iframe src={gamesLink} title="Vercel Games" className={classes.iframe}></iframe>
+        <Box p="xs" aria-label="LinkedIn" component="a" href={gamesLink} target="_blank" rel="noopener noreferrer">  
+            Visit the game site
+        </Box>
     </Stack>
 }
